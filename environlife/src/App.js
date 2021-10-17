c37950bf2e048d946d2179c0d64a76c935a29dee
Abdul Uddin <auddin431@gmail.com>
2021-10-16 23:20:16 -0400

    added google autocomplete

--- environlife/src/App.js
+++ environlife/src/App.js
@@ -6,21 +6,41 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
+import { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
+
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+
+const MapComponent = () => {
+  return <GooglePlacesAutocomplete apiKey={process.env.REACT_APP_GMAP_KEY} />;
+};
 
 function TestComponent(props) {
   return (
-    <img
-      style={{ maxWidth: "50%", maxHeight: "50%" }}
-      src={`data:image/png;base64, ${props.text}`}
-    />
+    <>
+      {props.text.map((im) => (
+        <div className="imDivItem">
+          <img
+            className="imageComponent"
+            src={`data:image/png;base64, ${im}`}
+          />
+        </div>
+      ))}
+      <h1>
+        Latitude = {props.ello[0]} Longitude = {props.ello[1]}
+      </h1>
+    </>
   );
 }
 
 function App() {
   const [location, setLocation] = useState("");
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
   const [show, setShow] = useState(0);
   const [start, setStart] = useState(2021);
-  const [image, setImage] = useState("");
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+  //const [image3, setImage3] = useState("");
   const [end, setEnd] = useState(2022);
 
   const testAPI = async () => {
@@ -30,12 +50,22 @@ function App() {
         //`http://192.168.1.135:5000/testing?location=${location}&start=${start}&end=${end}`
       );
       const response = await r.json();
-      setImage(response.picture);
+      setImage1(response.image1);
+      setImage2(response.image2);
+      //setImage3(response.image3);
       setShow(1);
       console.log(response);
     } catch (e) {
       console.error(e);
     }
+  };
+
+  const getLatLong = async () => {
+    const geoByAddy = await geocodeByAddress(location.label);
+    const getLL = await getLatLng(geoByAddy[0]);
+    setLatitude(getLL.lat);
+    setLongitude(getLL.lng);
+    setShow(1);
   };
 
   const changeLocation = (e) => {
@@ -54,9 +84,10 @@ function App() {
   };
 
   const test = () => {
-    console.log(location);
-    testAPI();
-    //s/etShow(1);
+    console.log(location.label);
+    getLatLong();
+    //testAPI();
+    //setShow(1);
   };
 
   return (
@@ -68,12 +99,20 @@ function App() {
             <Col xs={6}>
               <Form.Group className="mb-3">
                 <Form.Label>Location</Form.Label>
-                <Form.Control
+                <GooglePlacesAutocomplete
+                  apiKey="AIzaSyD3P3OPiWaxBOFk6metn9_GYWpGp4VuOUE"
+                  style={{ position: "absolute" }}
+                  selectProps={{
+                    location,
+                    onChange: setLocation,
+                  }}
+                />
+                {/* <Form.Control
                   type="text"
                   placeholder="Location"
                   value={location}
                   onChange={changeLocation}
-                />
+                /> */}
               </Form.Group>
             </Col>
 
@@ -101,10 +140,19 @@ function App() {
               </Form.Group>
             </Col>
           </Row>
-          <Button variant="success" onClick={test}>
-            Get Environment Stats
-          </Button>
-          <Row>{show === 1 && <TestComponent text={image} />}</Row>
+          <div>
+            <Button variant="success" onClick={test}>
+              Get Environment Stats
+            </Button>
+          </div>
+          <div className="imDiv">
+            {show === 1 && (
+              <TestComponent
+                ello={[latitude, longitude]}
+                text={[image1, image2]}
+              />
+            )}
+          </div>
         </Form>
       </Container>
     </div>
